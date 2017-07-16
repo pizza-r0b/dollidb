7295f58d11fcf6a7f9fba3f1f7a75691fa833c6e
Sean <seanpapanikolas@gmail.com>
2017-07-15 17:19:11 -0700

    revert back to autoinitialize doclient

--- src/utils/CreateDocClient.js
+++ src/utils/CreateDocClient.js
@@ -1,15 +1,9 @@
 import AWS from 'aws-sdk';
 
-let docClient;
-
 export default (config) => {
-  if (docClient) {
-    return docClient;
-  } else {
-    AWS.config.update({
-      region: 'us-east-1',
-      ...config,
-    });
-    return docClient = new AWS.DynamoDB.DocumentClient();
-  }
+  AWS.config.update({
+    region: 'us-east-1',
+    ...config,
+  });
+  return new AWS.DynamoDB.DocumentClient();
 };
